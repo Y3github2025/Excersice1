e13917bf9129b71ad41bf82d002bea39799a3ef7
Y3github2025 <topvat292@gmail.com>
2024-06-20 18:21:01 +0700

    commited

---
+++ src/main/java/com/vatra/midterm/LockClass.java
@@ -0,0 +1,13 @@
+package com.vatra.midterm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockClass {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LockClass.class, args);
+    }
+
+}
